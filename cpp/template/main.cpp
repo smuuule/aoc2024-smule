ce44b6a5a90316209073d82cb63ab29a0035717c
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2024-12-01 17:22:18 +0100

    Initial commit

---
+++ cpp/template/main.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int partone(std::vector<std::string> lines) { return -1; }
+
+int parttwo(std::vector<std::string> lines) { return -1; }
+
+int main(int argc, char *argv[]) {
+  std::string file_string{"ex.txt"};
+  if (argc > 1) {
+    file_string = argv[1];
+  } else {
+    std::cout << "Input file not supplied! Using ex.txt\n";
+  }
+  std::ifstream file(file_string);
+  std::vector<std::string> lines;
+  if (file.is_open()) {
+    std::string line;
+    while (std::getline(file, line)) {
+      lines.push_back(line);
+    }
+    file.close();
+  } else {
+    std::cerr << "Unable to open file" << std::endl;
+  }
+
+  int res1 = partone(lines);
+  std::cout << "Part 1 Result: " << res1 << '\n';
+  int res2 = parttwo(lines);
+  std::cout << "Part 2 Result: " << res2 << '\n';
+
+  return 0;
+}
