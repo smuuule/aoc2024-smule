489fc96e4f351a637bef749a66223924a4e5d7ed
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2024-12-02 10:33:11 +0100

    template update

--- cpp/template/main.cpp
+++ cpp/template/main.cpp
@@ -3,6 +3,10 @@
 #include <string>
 #include <vector>
 
+std::vector<std::string> toStrVector(std::string in, const std::string &sep);
+std::vector<int> toIntVector(std::string in, const std::string &sep);
+std::vector<long> toLongVector(std::string in, const std::string &sep);
+
 int partone(std::vector<std::string> lines) { return -1; }
 
 int parttwo(std::vector<std::string> lines) { return -1; }
@@ -32,4 +36,46 @@ int main(int argc, char *argv[]) {
   std::cout << "Part 2 Result: " << res2 << '\n';
 
   return 0;
+}
+
+std::vector<std::string> toStrVector(std::string in, const std::string &sep) {
+  std::vector<std::string> out;
+
+  // Find first occurrence of the delimiter
+  auto pos = in.find(sep);
+
+  // While there are still delimiters in the
+  // string
+  while (pos != std::string::npos) {
+
+    // Extracting the substring up to the
+    // delimiter
+    out.push_back(in.substr(0, pos));
+
+    // Erase the extracted part from the
+    // original string
+    in.erase(0, pos + sep.length());
+
+    // Find the next occurrence of the
+    // delimiter
+    pos = in.find(sep);
+  }
+  out.push_back(in);
+  return out;
+}
+std::vector<int> toIntVector(std::string in, const std::string &sep) {
+  std::vector<std::string> strVector = toStrVector(in, sep);
+  std::vector<int> out;
+  for (std::string str : strVector) {
+    out.push_back(std::stoi(str));
+  }
+  return out;
+}
+std::vector<long> toLongVector(std::string in, const std::string &sep) {
+  std::vector<std::string> strVector = toStrVector(in, sep);
+  std::vector<long> out;
+  for (std::string str : strVector) {
+    out.push_back(std::stol(str));
+  }
+  return out;
 }
