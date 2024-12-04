441c5dfca1d5a467afdc09557a7b00135dda237c
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2024-12-04 01:41:54 +0100

    update template

--- cpp/template/main.cpp
+++ cpp/template/main.cpp
@@ -1,11 +1,8 @@
-#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-std::vector<std::string> toStrVector(std::string in, const std::string &sep);
-std::vector<int> toIntVector(std::string in, const std::string &sep);
-std::vector<long> toLongVector(std::string in, const std::string &sep);
+#include "../UtilsLib/UtilsLib.h"
 
 int partone(std::vector<std::string> lines) { return -1; }
 
@@ -18,17 +15,8 @@ int main(int argc, char *argv[]) {
   } else {
     std::cout << "Input file not supplied! Using ex.txt\n";
   }
-  std::ifstream file(file_string);
-  std::vector<std::string> lines;
-  if (file.is_open()) {
-    std::string line;
-    while (std::getline(file, line)) {
-      lines.push_back(line);
-    }
-    file.close();
-  } else {
-    std::cerr << "Unable to open file" << std::endl;
-  }
+
+  std::vector<std::string> lines = readFile(file_string);
 
   int res1 = partone(lines);
   std::cout << "Part 1 Result: " << res1 << '\n';
@@ -36,46 +24,4 @@ int main(int argc, char *argv[]) {
   std::cout << "Part 2 Result: " << res2 << '\n';
 
   return 0;
-}
-
-std::vector<std::string> toStrVector(std::string in, const std::string &sep) {
-  std::vector<std::string> out;
-
-  // Find first occurrence of the delimiter
-  auto pos = in.find(sep);
-
-  // While there are still delimiters in the
-  // string
-  while (pos != std::string::npos) {
-
-    // Extracting the substring up to the
-    // delimiter
-    out.push_back(in.substr(0, pos));
-
-    // Erase the extracted part from the
-    // original string
-    in.erase(0, pos + sep.length());
-
-    // Find the next occurrence of the
-    // delimiter
-    pos = in.find(sep);
-  }
-  out.push_back(in);
-  return out;
-}
-std::vector<int> toIntVector(std::string in, const std::string &sep) {
-  std::vector<std::string> strVector = toStrVector(in, sep);
-  std::vector<int> out;
-  for (std::string str : strVector) {
-    out.push_back(std::stoi(str));
-  }
-  return out;
-}
-std::vector<long> toLongVector(std::string in, const std::string &sep) {
-  std::vector<std::string> strVector = toStrVector(in, sep);
-  std::vector<long> out;
-  for (std::string str : strVector) {
-    out.push_back(std::stol(str));
-  }
-  return out;
 }
